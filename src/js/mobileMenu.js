27e11e3b0abcbdbcd3a07027d43d3bbddae25af0
colinxr <colinxr@gmail.com>
2018-05-29 15:32:27 -0400

    Enhancement: Keep mobile nav toggle if window resizes to tablet or bigger

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	var scrollTimeout;
-	var navBar = $('nav');
+	var navBar = $('nav.main');
 	var mobileNav = $('nav.mobile');
 	var navList = $('.nav__list');
 	var menuBtn = $('.menu');
+
+	var resizeTimeout;
 
 	$(window).scroll(function(e) {
 		if ($(document).scrollTop() > 100 && fullPageScroll === false) {
@@ -11,12 +13,20 @@ $(document).ready(function() {
 		}
 	});
 
+	$(window).resize(function(e) {
+		clearTimeout(resizeTimeout);
+		resizeTimeout = setTimeout(function() {
+			resizeNav();
+		}, 250);
+	});
+
 	menuBtn.on('click', function() {
-		console.log('check');
+
 		$(this).toggleClass('open');
 		mobileNav.toggleClass('active');
 
 		setMenuBtnText($(this));
+		resizeNav();
 	});
 
 	function setMenuBtnText(el) {
@@ -25,5 +35,18 @@ $(document).ready(function() {
 		} else {
 			el.children().text('Open City Guides');
 		}
+	}
+
+	function resizeNav() {
+		var vpWidth = $(window).width();
+
+		if (vpWidth > 800 && mobileNav.hasClass('active')) {
+			menuBtn.css('display', 'block');
+		}
+
+		if (vpWidth > 800 && !mobileNav.hasClass('active')) {
+			menuBtn.css('display', 'none');
+		}
+
 	}
 });
